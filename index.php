ebfbf69d168674b7654e5f05ef2949b25222afdf
danuson <danuson.o@gmail.com>
2022-03-12 03:55:52 +0700

    FIXE: login_connect

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php 
-
 session_start();
 
-if(!isset($_SESSION['username'])){
-    $_SESSION['msg'] = "You must log in first";
-    header('location: login.php');
-}
-
-if(isset($_SESSION['logout'])){
-    session_destroy();
-    unset($_SESSION['username']);
-    header('location: login.php');
-}
-
+if(isset($_SESSION['id']) && isset($_SESSION['username'])){
+    
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +12,7 @@ if(isset($_SESSION['logout'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project-fit</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
@@ -30,12 +21,17 @@ if(isset($_SESSION['logout'])){
 </div>
 
 <div class="content">
-    <h1>Hello World!</h1>
+    <h1>Hello World!, <?php echo $_SESSION['firstname']; ?></h1>
+    <a href="logout.php">Logout</a>
 </div>
-    
-<?php  if(isset($_SESSION['username'])): ?>
-        <p>ยินดีต้อนรับ <strong><?php echo $_SESSION['username']; ?></strong></p>
-        <p><a href="index.php?logout='1'">Logout</a></p>
-    <?php endif ?>
+
 </body>
 </html>
+
+<?php
+}else{
+    header("Location: login.php");
+    exit();
+}
+
+?>
--- login.php
+++ login.php
@@ -10,6 +10,7 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -22,14 +23,17 @@
         <div class="row">
             <div class="col-md-12">
                 <form action="login_connect.php" method="post">
+                    <?php if(isset($_GET['error'])) { ?>
+                        <p class="error"><?php echo $_GET['error']; ?></p>
+                    <?php } ?>
                     <div class="form-group">
                         <label for="text"><b>Username</b></label>
-                        <input type="text" class="form-control" id="email" name="username"
+                        <input type="text" class="form-control" name="username"
                             placeholder="อีเมลหรือชื่อผู้ใช้งาน">
                     </div><br>
                     <div class="form-group">
                         <label for="password"><b>Password</b></label>
-                        <input type="password" class="form-control" id="password" name="password"
+                        <input type="password" class="form-control" name="password"
                             placeholder="รหัสผ่าน">
                     </div><br>
                     <button type="submit" name="login_user" class="btn btn-primary w-100">เข้าสู่ระบบ</button>
